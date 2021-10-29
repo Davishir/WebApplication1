50816eb086745c8286c428559f8d4699b6035683
Ширыкалова Дарья <shirykalova02@yandex.com>
2021-10-29 11:52:32 +0500

    Добавление функции управления ролями, добавление RoleInitializer

---
+++ WebApplication1/Models/RoleInitializer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "shirykalova02@yandex.ru";
+            string password = "_Davis20112002";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("registeredUser") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("registeredUser"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User
+                {
+                    Email = adminEmail,
+                    UserName = adminEmail,
+                    LastName = "Ширыкалова",
+                    FirstName = "Даша",
+                    Patronymic = "Витальевна",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
+    
